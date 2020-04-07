20e28a9c85c7a213653f3ede1522d271590bc6d2
divanshusingla <singladivanshu007@gmail.com>
2020-04-07 11:50:01 +0530

    assign

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'assignmentOne'`, () => {
+  it(`should have as title 'assignOne'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('assignmentOne');
+    expect(app.title).toEqual('assignOne');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('assignmentOne app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('assignOne app is running!');
   });
 });
